ca3b61409e3f081eaef2868a22cc221496307b7c
Robert Jones <robert.g.jones@gmail.com>
2017-06-12 16:51:59 +0100

    Rename feature test file

--- spec/features/features.rb
+++ spec/features/features.rb
@@ -1,11 +1,17 @@
 require 'account'
+
 describe 'Feature Tests: ' do
   account = Account.new
 
   describe 'Outputs a statement' do
-    it 'with date' do
+    it 'with amount' do
       account.deposit(10)
       expect { account.statement }.to output("£10\n").to_stdout
+    end
+
+    it 'with date' do
+      account.deposit(10)
+      expect { account.statement }.to output('12-06-2017').to_stdout
     end
   end
 end
