284251b3c2d65953c3bc15f2894b4fc5dcd28186
Robert Jones <robert.g.jones@gmail.com>
2017-06-12 14:53:12 +0100

    simple balance, deposit and withdraw functions

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,25 @@
 class Account
 
+  attr_reader :account_balance
+
+  def initialize
+    @account_balance = 0
+  end
 
   def balance
-    0
+    account_balance
   end
+
+  def deposit(amount)
+    self.account_balance += amount
+  end
+
+  def withdraw(amount)
+    self.account_balance -= amount
+  end
+
+  private
+
+  attr_writer :account_balance
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,6 +7,17 @@ describe Account do
     it "has a zero balance" do
       expect(account.balance).to eq(0)
     end
-    
+  end
+
+  describe "#deposit" do
+    it "increases balance" do
+      expect{ account.deposit(10) }.to change{ account.balance }.by(10)
+    end
+  end
+
+  describe "#withdraw" do
+    it "decreases balance" do
+      expect{ account.withdraw(10) }.to change{ account.balance }.by(-10)
+    end
   end
 end
